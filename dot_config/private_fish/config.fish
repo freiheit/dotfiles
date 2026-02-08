a48630a9cb254ccea76c7fcf7f13d5bbfd4ad488
freiheit <git@freiheit.wtf>
2026-02-07 22:41:25 -0800

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,4 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# https://github.com/catppuccin/fish
+fish_config theme choose "Catppuccin Mocha"
+
 starship init fish | source

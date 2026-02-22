8a97fae1307893ea88aaf52bb1a49d2ac52c1010
freiheit <git@freiheit.wtf>
2026-02-22 15:55:35 -0800

    Update .bashrc Update .config/fish/config.fish Update .ssh/config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set --local OP_AUTH_SOCK "$HOME/.1password/agent.sock"
+if test -S $OP_AUTH_SOCK -a -r $OP_AUTH_SOCK -a -w $OP_AUTH_SOCK
+    set --global --export SSH_AUTH_SOCK "$OP_AUTH_SOCK"
+end
+
 # https://github.com/catppuccin/fish
 fish_config theme choose "Catppuccin Mocha"
 

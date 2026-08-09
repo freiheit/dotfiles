f6bf29b71f12ac195e14502a46c20304f59e9a1b
freiheit <git@freiheit.wtf>
2026-08-08 20:16:44 -0700

    chore: drop dead shell config

--- dot_bashrc.d/prompt.sh
+++ dot_bashrc.d/prompt.sh
@@ -8,15 +8,4 @@ if [ -e ~/.local/share/liquidprompt/liquidprompt ]; then
         lp_theme powerline_full
     fi
 
-elif which starship &>/dev/null; then
-
-    function set_win_title() {
-        echo -ne "\033]0; $(basename "$PWD") $USER@$HOSTNAME\007"
-    }
-
-    starship_precmd_user_func="set_win_title"
-
-    eval -- "$(starship init bash --print-full-init)"
-    eval "$(starship completions bash)"
-
 fi
